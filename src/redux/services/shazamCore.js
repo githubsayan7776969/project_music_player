5f5515d11cabf08ebb9ac58daaed7ffda24c5c99
githubsayan7776969 <68245752+githubsayan7776969@users.noreply.github.com>
2022-12-10 19:58:21 +0530

    WFD

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -15,7 +15,7 @@ export const shazamCoreApi = createApi({
     getSongsByGenre: builder.query({ query: (genre) => `/v1/charts/genre-world?genre_code=${genre}` }),
     getSongsByCountry: builder.query({ query: (countryCode) => `/v1/charts/country?country_code=${countryCode}` }),
     getSongsBySearch: builder.query({ query: (searchTerm) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
-    getArtistDetails: builder.query({ query: (artistId) => `/v2/artists/details?artist_id=${artistId}` }),
+    getArtistDetails: builder.query({ query: (artistId) => '/v2/artists/details?artist_id=${artistId}' }),
     getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}` }),
   }),
